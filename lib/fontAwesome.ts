5027bb74bb13d68f4334f6d9659c842e7d67c875
elramus <luke.ramus@gmail.com>
2019-11-04 09:52:07 -0600

    Reorganize FA icons

---
+++ lib/fontAwesome.ts
@@ -0,0 +1,33 @@
+import {
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons'
+import {
+  faAngleDown,
+  faAngleUp,
+} from '@fortawesome/pro-regular-svg-icons'
+import {
+  faArrowDown,
+  faCaretUp,
+  faCaretDown,
+  faCheck,
+  faCog,
+  faEnvelope,
+  faExternalLinkSquareAlt,
+  faSearch,
+} from '@fortawesome/pro-solid-svg-icons'
+
+const faIcons = [
+  faAngleDown,
+  faAngleUp,
+  faArrowDown,
+  faCaretUp,
+  faCaretDown,
+  faCheck,
+  faCog,
+  faEnvelope,
+  faExternalLinkSquareAlt,
+  faSearch,
+  faGithub,
+]
+
+export default faIcons
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,41 +3,13 @@ import App from 'next/app'
 import { Provider } from 'react-redux'
 import withRedux from 'next-redux-wrapper'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import {
-  faGithub,
-} from '@fortawesome/free-brands-svg-icons'
-import {
-  faAngleDown,
-  faAngleUp,
-} from '@fortawesome/pro-regular-svg-icons'
-import {
-  faArrowDown,
-  faCaretUp,
-  faCaretDown,
-  faCheck,
-  faCog,
-  faEnvelope,
-  faExternalLinkSquareAlt,
-  faSearch,
-} from '@fortawesome/pro-solid-svg-icons'
 import Page from '../components/Page'
 import '../normalize.css'
 import GlobalStyles from '../lib/globalStyles'
 import { initializeStore } from '../store'
+import faIcons from '../lib/fontAwesome'
 
-library.add(
-  faAngleDown,
-  faAngleUp,
-  faArrowDown,
-  faCaretUp,
-  faCaretDown,
-  faCheck,
-  faCog,
-  faEnvelope,
-  faExternalLinkSquareAlt,
-  faSearch,
-  faGithub,
-)
+library.add(...faIcons)
 
 class MyApp extends App<any> {
   render() {
